9e79232840fc9e6a8d610701cb6787fce4492bd4
xrito <88393376+xrito@users.noreply.github.com>
2021-10-11 01:47:15 +0400

    Rev_1.1

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,7 +8,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', cache_page(20)(views.index), name='index'),
-#     path('', views.index, name='index'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
