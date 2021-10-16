7741000818c24cc90d5debd1c4b957e9929a4550
xrito <88393376+xrito@users.noreply.github.com>
2021-10-16 19:36:45 +0400

    Final 3

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -7,10 +7,13 @@ SECRET_KEY = 'a-)thsn1q1%=+!yer80sp9r*qcgpt658d%nb3e-(0qr5v!k&p$'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver',
-                 '127.0.0.1',
-                 'localhost',
-                 ]
+ALLOWED_HOSTS = [
+    'testserver',
+    '127.0.0.1',
+    'localhost',
+    'www.harkort.pythonanywhere.com',
+    'harkort.pythonanywhere.com',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
